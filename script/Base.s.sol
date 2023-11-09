a25e21be755d3e29aebdf183c51d231f2b6ba78d
tasiby <tuanhoang.uit@gmail.com>
2023-11-09 17:21:00 +0700

    init

---
+++ script/Base.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import { console2, Script } from "forge-std/Script.sol";
+import { IScript } from "script/interfaces/IScript.sol";
+import { ErrorHandler } from "script/libraries/ErrorHandler.sol";
+
+abstract contract BaseScript is Script, IScript {
+    using ErrorHandler for *;
+
+    modifier broadcast() {
+        vm.startBroadcast();
+        _;
+        vm.stopBroadcast();
+    }
+
+    function run(bytes calldata callData) external {
+        (bool success, bytes memory returnOrRevertData) = address(this).delegatecall(callData);
+        success.handleRevert(returnOrRevertData);
+    }
+}
