daebc491f2d595e03bca339cbd06d6ccc5ee5bf7
cgccld <tasibii.web3@gmail.com>
2024-07-10 13:57:54 +0700

    feat: explorer url

--- src/Base.s.sol
+++ src/Base.s.sol
@@ -21,4 +21,29 @@ abstract contract BaseScript is Script {
         string memory formattedLog = string.concat("> ", description, "...");
         console2.log(StdStyle.blue(formattedLog));
     }
+
+    function _explorerUrl() internal returns (string memory) {
+        uint256 chainId = block.chainid;
+
+        // etherscan
+        if (chainId == 1) return "https://etherscan.io/";
+        if (chainId == 11155111) return "https://sepolia.etherscan.io/";
+        // bscscan
+        if (chainId == 56) return "https://bscscan.com/";
+        if (chainId == 97) return "https://testnet.bscscan.com/";
+        // polygon
+        if (chainId == 137) return "https://polygonscan.com/";
+        if (chainId == 80002) return "https://amoy.polygonscan.com/";
+        // avalanche
+        if (chainId == 43114) return "https://snowtrace.io/";
+        if (chainId == 43113) return "https://testnet.snowtrace.io/";
+        // op
+        if (chainId == 10) return "https://optimistic.etherscan.io/";
+        if (chainId == 11155420) return "https://sepolia-optimism.etherscan.io/";
+
+        if (chainId == 42161) return "https://arbiscan.io/";
+        if (chainId == 421614) return "https://sepolia.arbiscan.io/";
+
+        else return "";
+    }
 }
